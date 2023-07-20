a4a82fef8f77209d84449c39e50e17715f5a3f2e
YiZou89 <zouyi89@outlook.com>
2023-07-20 15:48:55 +0800

    test 添加测试

--- example/main.go
+++ example/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"zouyi/minidb"
 )
 
 func main() {
-	db, err := minidb.Open("/tmp/minidb")
+	db, err := minidb.Open("./tmp/minidb")
 	if err != nil {
 		panic(err)
 	}
@@ -15,15 +16,20 @@ func main() {
 		key   = []byte("dbname")
 		value = []byte("minidb")
 	)
-
 	err = db.Put(key, value)
 	if err != nil {
 		panic(err)
 	}
+	for i := 0; i < 1000; i++ {
 
+		err = db.Put([]byte("key"+strconv.Itoa(i)), []byte("value"+strconv.Itoa(i)))
+		if err != nil {
+			panic(err)
+		}
+	}
 	fmt.Printf("1. put kv successfully, key: %s, value: %s.\n", string(key), string(value))
 
-	cur, err := db.Get(key)
+	cur, err := db.Get([]byte("key50"))
 	if err != nil {
 		panic(err)
 	}
